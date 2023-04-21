71cd4e5d392dade23c4cb43c1a6d4b41b3e33221
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2023-04-21 15:22:28 +0300

    Fix

--- js/modal-console.js
+++ js/modal-console.js
@@ -1,12 +1,11 @@
+;(() => {
+  document.querySelector(".js-speaker-form").addEventListener("submit", e => {
+    e.preventDefault()
 
-        (() => {
-            document.querySelector('.js-speaker-form').addEventListener('submit', e => {
-                e.preventDefault();
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`),
+    )
 
-                new FormData(e.currentTarget).forEach((value, name) =>
-                    console.log(`${name}: ${value}`),
-                );
-
-                e.currentTarget.reset();
-            });
-        })();
+    e.currentTarget.reset()
+  })
+})()
